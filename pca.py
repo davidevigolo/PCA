d65e23c597aa68f30d0806afa009f0346b2e8728
Matteo Mantoan <mattemant@gmail.com>
2025-03-24 19:21:58 +0100

    fix pca, TODO comments

--- pca.py
+++ pca.py
@@ -1,39 +1,35 @@
-# Realizzato seguendo https://builtin.com/data-science/step-step-explanation-principal-component-analysis
-
 import numpy as np
 
-# Data una matrice costruisce la matrice di covarianza
 def build_cov_mat(data):
-    data = np.array(data, dtype=float)
-    cov_matrix = np.cov(data, rowvar=False)
-    return cov_matrix
+    return np.cov(data, rowvar=False)
 
-# restituisce la matrice trasposta degli autovettori U^t della decomposizione spettrale di una data matrice
 def spectral_decomposition(matrix):
     eigvals, eigvecs = np.linalg.eig(matrix)
-    return eigvecs.T,eigvals
+    return eigvecs.T, eigvals  # Rows are eigenvectors
 
-# Data una matrice di dati restituisce la matrice dei dati trasformata in base alle prime due componenti principali
 def pca(data):
-    cov_matrix = build_cov_mat(data)
-    eigvecs, eigvals = spectral_decomposition(cov_matrix)
-
-    # ordina gli autovettori in ordine decrescente
-    sorted_indices = np.argsort(eigvals)[::-1]
-    eigvecs = eigvecs[:, sorted_indices]
-    eigvals = eigvals[sorted_indices]
-
-    data_matrix = np.array(data)
-    # seleziona le prime due componenti principali, sono ordinati in ordine decrescente quindi i primi due sono i maggiori
-    eigvecs = eigvecs[:, :2]
-
-    # standardizzazione dei dati
-    standard_deviation = np.std(data, axis=0)
-    data_mean = data_matrix - np.mean(data, axis=0)
-    data_matrix_std = data_mean / standard_deviation
-
-    # proiezione dei dati sulla base degli autovettori scelti
-    rescaled_data = (eigvecs.T @ data_matrix_std.T)
-
-    return rescaled_data.T
+    data = np.array(data, dtype=float)
     
+    # 1. Standardize FIRST
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0, ddof=1)  # Use sample std (ddof=1)
+    data_std = (data - mean) / std
+    
+    # 2. Compute covariance of standardized data
+    cov_matrix = build_cov_mat(data_std)
+    
+    # 3. Spectral decomposition
+    eigvecs, eigvals = spectral_decomposition(cov_matrix)
+    
+    # 4. Sort eigenvectors by eigenvalues (descending)
+    sorted_indices = np.argsort(eigvals)[::-1]
+    eigvecs = eigvecs[sorted_indices, :]  # Sort rows
+    eigvals = eigvals[sorted_indices]
+    
+    # 5. Select top 2 eigenvectors (rows)
+    top_eigvecs = eigvecs[:2, :]
+    
+    # 6. Project standardized data
+    projected_data = (top_eigvecs @ data_std.T).T
+    
+    return projected_data
